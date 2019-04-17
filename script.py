a78911948b0cf1ae5fdd68b6f947b18b6b06ec56
MishaVernik <nodus1.5.3@gmail.com>
2019-04-18 01:49:15 +0300

    WOOW13

--- script.py
+++ script.py
@@ -70,10 +70,71 @@ def get_ses():
     html = html.replace("{number_repeats}", str(number_of_repeats))
     html = html.replace("{link}", str(link))
     html = html.replace("{sleeping}", str(sleeping_time))
-    t = Timer(10.0, get_ses)
+    t = Timer(10.0, get_ses_timer)
     t.start()
     #return html
 
+@app.route('/btn_find')
+def get_ses_timer():
+    global link
+    global sleeping_time
+    global counter    
+    global number_of_repeats
+
+    couter = 1
+    if number_of_repeats == 0:
+        number_of_repeats = int(request.form['number'])
+    if sleeping_time == 0:
+        sleeping_time = float(request.form['sleeping'])
+    if link == "":
+        s = request.form['text']
+        link = s
+    
+    #response = urllib.request.urlopen(request.form['text'])    
+    while number_of_repeats > 0:
+        if (sleeping_time*(counter+1) > 25):
+            break
+        counter +=1
+        number_of_repeats -= 1  
+        send_request(link)
+        print('#'*40)
+        print(number_of_repeats)
+        print('#'*40)        
+        time.sleep(sleeping_time)
+    html = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Clicker chooser Online</title>
+</head>
+<body>
+<h2 align="center">Welcome to the Clicker.online!</h2>
+<form method="POST" action="">
+    <h3>Link</h3>
+    <p align="center">
+        <input name="text" type="text" value="{link}">
+    </p>
+    <h3>Number of repeats</h3>
+     <p align="center">
+        <input name="number" type="text" value="{number_repeats}">
+    </p>
+    <h3>Sleeping time</h3>
+     <p align="center">
+        <input name="sleeping" type="text" value="{sleeping}">
+    </p>
+</form>
+</body>
+</html>
+'''
+    if number_of_repeats < 0:        
+        return "Success"
+    
+    html = html.replace("{number_repeats}", str(number_of_repeats))
+    html = html.replace("{link}", str(link))
+    html = html.replace("{sleeping}", str(sleeping_time))
+    t = Timer(10.0, get_ses_timer)
+    t.start()
+    #return html
  
         
 def send_request(s):
