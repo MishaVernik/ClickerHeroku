73d17d75e0fc79dd5dae916c802c664b1dfc9eb9
MishaVernik <nodus1.5.3@gmail.com>
2019-04-17 23:13:51 +0300

    qeqwe

--- script.py
+++ script.py
@@ -4,7 +4,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def source():
-    with open("index.html", 'r') as f:
-        html = f
-        
+    html = '''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Route chooser Online</title>
+</head>
+<body>
+<h2 align="center">Welcome to the railway.online!</h2>
+<form method="post" action="btn_add_person">
+    <p align="center">
+        <input type="submit" value="Add person">
+    </p>
+</form>
+<form method="post" action="btn_add_route">
+    <p align="center">
+        <input type="submit" value="Add route">
+    </p>
+</form>
+<form method="post" action="btn_find_route">
+    <p align="center">
+        <input type="submit" value="Find route">
+    </p>
+</form>
+</body>
+</html>
+'''
     return html
